a425b1af35771687df0eb822a354adcc9bf2631f
DioguinhoGamer3D <diogo.silva@dcx.ufpb.br>
2025-02-04 10:38:07 -0300

    Adicionei o projeto do posto

---
+++ src/main/java/br/ufpb/dcx/diogo/posto/AtendimentoJaCadastradoException.java
@@ -0,0 +1,8 @@
+package br.ufpb.dcx.diogo.posto;
+
+public class AtendimentoJaCadastradoException extends  Exception {
+
+    public AtendimentoJaCadastradoException(String msg){
+        super(msg);
+    }
+}
