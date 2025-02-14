0e7c775598cad8c8e02f05a652b92c002fa14670
Diogo <diogo.silva@dcx.ufpbr.br>
2025-02-14 17:05:04 -0300

    Terminei o SistemaLojaRoupasPOOTest

--- src/test/java/br/ufpb/dcx/diogo/lojaroupas/SistemaLojaRoupasPOOTest.java
+++ src/test/java/br/ufpb/dcx/diogo/lojaroupas/SistemaLojaRoupasPOOTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class SistemaLojaRoupasPOOTest {
+public class SistemaLojaRoupasPOOTest {
     @Test
     public void testaCadastroDeRoupa(){
         SistemaLojaRoupasPOO sistema = new SistemaLojaRoupasPOO();
@@ -14,9 +14,9 @@ class SistemaLojaRoupasPOOTest {
             Roupa r = sistema.pesquisaRoupa("001");
             assertEquals(10, r.getQuantidade());
             sistema.cadastraRoupa("002","Camisa vermelha feminina", Tamanho.M, 2);
-            assertEquals(2, sistema.pesquisaRoupasComDescricaoComecandoCom("Camisa"));
+            assertEquals(2, sistema.pesquisaRoupasComDescricaoComecandoCom("Camisa").size());
             sistema.alteraQuantidadeDeRoupaNoEstoque("001", "9");
-            assertEquals(9,sistema.pesquisaRoupa("001"));
+            assertEquals(9,sistema.pesquisaRoupa("001").getQuantidade());
         } catch (RoupaJaExisteException | RoupaInexistenteException e){
             fail("Nâo deveria lançar exceção");
         }
