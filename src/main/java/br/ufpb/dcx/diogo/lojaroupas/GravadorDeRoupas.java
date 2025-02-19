1940f0d0716f89da7b7483e259e08e12aefc63d6
diogo.silva <diogo.silva@dcx.ufpb.br>
2025-02-19 09:12:58 -0300

    completei o programaGravacao

--- src/main/java/br/ufpb/dcx/diogo/lojaroupas/GravadorDeRoupas.java
+++ src/main/java/br/ufpb/dcx/diogo/lojaroupas/GravadorDeRoupas.java
@@ -6,37 +6,41 @@ import java.util.Collection;
 
 public class GravadorDeRoupas {
 
+
     private String arquivoRoupas;
 
     public GravadorDeRoupas(){
-        this.arquivoRoupas="roupas.dat";
-    }
-    public Collection<Roupa> recuperaRoupas() throws IOException{
-        try {
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream(this.arquivoRoupas));
-            Collection<Roupa> roupasAchadas = (ArrayList<Roupa>)in.readObject();
-            return roupasAchadas;
-        }catch (ClassNotFoundException | ClassCastException e){
-            throw new IOException(e);
-        }
+        this.arquivoRoupas = "roupas.dat";
     }
 
-    public void gravaRoupas(Collection <Roupa> roupas) throws IOException {
+    public Collection<Roupa> recuperaRoupas() throws IOException {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(this.arquivoRoupas))) {
+            Collection<Roupa> roupasAchadas =  (ArrayList<Roupa>) in.readObject();
+            return roupasAchadas;
+        } catch (ClassNotFoundException | ClassCastException e) {
+            throw new IOException(e);
+        }
+
+    }
+
+    public void gravaRoupas(Collection<Roupa> roupas) throws IOException {
         ArrayList<Roupa> roupasArrayList = new ArrayList<>();
         roupasArrayList.addAll(roupas);
-//        ObjectOutputStream out = null;
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(arquivoRoupas))){
+            out.writeObject(roupasArrayList);
+        }
+
+        //        ObjectOutputStream out = null;
 //        try {
-//            new ObjectOutputStream(new FileOutputStream(arquivoRoupas));
+//            out = new ObjectOutputStream(new FileOutputStream(arquivoRoupas));
 //            out.writeObject(roupasArrayList);
 //        } finally {
-//            if (out != null) {
+//            if (out!=null){
 //                out.close();
 //            }
 //        }
-        try {
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(this.arquivoRoupas));
-        } catch (IOException e) {
-            throw new IOException(e);
-        }
+
     }
+
+
 }
